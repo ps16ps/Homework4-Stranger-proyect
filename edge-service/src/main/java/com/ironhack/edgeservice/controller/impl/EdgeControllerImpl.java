ce39398a78ee36d4015d9da7fbceecf7394cb6fe
Alba Mesa <alba.mesa.garrido@gmail.com>
2022-07-12 11:28:03 +0200

    get by id included in edge

--- edge-service/src/main/java/com/ironhack/edgeservice/controller/impl/EdgeControllerImpl.java
+++ edge-service/src/main/java/com/ironhack/edgeservice/controller/impl/EdgeControllerImpl.java
@@ -6,6 +6,7 @@ import com.ironhack.edgeservice.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -58,6 +59,36 @@ public class EdgeControllerImpl implements EdgeController {
     @ResponseStatus(HttpStatus.OK)
     public List<Account> getAllAccounts(){
         return accountClient.getAllAccount();
+    }
+
+    @GetMapping("/leads/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Lead getLeadsById(@PathVariable Long id){
+        return leadClient.getLeadById(id);
+    }
+
+    @GetMapping("/sales-rep/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public SalesRep getSalesRepsById(@PathVariable Long id) {
+        return salesRepClient.getSalesRepById(id);
+    }
+
+    @GetMapping("/contacts/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Contact getContactsById(@PathVariable Long id) {
+        return contactClient.findById(id);
+    }
+
+    @GetMapping("/opportunities/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Opportunity getOpportunitiesById(@PathVariable Long id){
+        return opportunityClient.getOpportunityById(id);
+    }
+
+    @GetMapping("/accounts/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Account getAccountsById(@PathVariable Long id){
+        return accountClient.getAccountById(id);
     }
 
     //Posts
